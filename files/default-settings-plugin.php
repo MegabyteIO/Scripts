93d4cb07efd6b4f0cfd72a5c1c7cba87583acf8c
bzizzle <brian@enviedsolutions.com>
2013-11-08 05:03:02 -0500

    Update default-settings-plugin.php
    Added function notes on activating/deactivating plugins

--- files/default-settings-plugin.php
+++ files/default-settings-plugin.php
@@ -11,18 +11,20 @@ Author URI: http://poor.io/
 function poor_io_defaults()
 {
     $o = array(
-        'avatar_default'            => 'blank',
+        'avatar_default'            => 'mystery',
         'avatar_rating'             => 'G',
-        'category_base'             => '/thema',
-        'comment_max_links'         => 0,
-        'comments_per_page'         => 0,
-        'date_format'               => 'd.m.Y',
-        'default_ping_status'       => 'closed',
+        'category_base'             => '/%category%/',
+        'comments_notify'           => 0,
+        'comment_max_links'         => 1,
+        'comments_per_page'         => 10,
+        'date_format'               => 'j. F Y, H:i',
+        'default_ping_status'       => 'open',
         'default_post_edit_rows'    => 30,
+        'gmt_offset'                => -4,
         'links_updated_date_format' => 'j. F Y, H:i',
-        'permalink_structure'       => '/%year%/%postname%/',
-        'rss_language'              => 'de',
-        'timezone_string'           => 'Etc/GMT-1',
+        'permalink_structure'       => '/%post_id%/%postname%',
+        'rss_language'              => 'en',
+        'tag_base'                  => '/tag/',
         'use_smilies'               => 0,
     );
 
@@ -37,5 +39,40 @@ function poor_io_defaults()
 
     return;
 }
-register_activation_hook(__FILE__, 'poor_io_defaults');
+register_activation_hook(__FILE__, 'poor_io_defaults'); // Fires when plugin is activated
 ?>
+
+
+
+
+
+
+
+function toggle_plugin() {
+
+	// Full path to WordPress from the root
+	$wordpress_path = '/full/path/to/wordpress/';
+
+	// Absolute path to plugins dir
+	$plugin_path = $wordpress_path.'wp-content/plugins/';
+
+	// Absolute path to your specific plugin
+	$my_plugin = $plugin_path.'my_plugin/my_plugin.php';
+
+	// Check to see if plugin is already active
+	if(is_plugin_active($my_plugin)) {
+
+		// Deactivate plugin
+		// Note that deactivate_plugins() will also take an
+		// array of plugin paths as a parameter instead of
+		// just a single string.
+		deactivate_plugins($my_plugin);
+	}
+	else {
+
+		// Activate plugin
+		activate_plugin($my_plugin);
+	}
+}
+
+
